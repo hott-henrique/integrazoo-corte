92d9d8512ca498bb64b9d6f42f93d7c58cb2a8ac
Henrique Hott <cs.henriquemchott@gmail.com>
2025-04-03 12:42:18 -0300

    refactor remove not needed pages.

--- lib/ui/screens.dart
+++ lib/ui/screens.dart
@@ -10,8 +10,6 @@ export 'package:integrazoo/ui/screens/bovine_screen/bovine_screen.dart';
 export 'package:integrazoo/ui/screens/relatories/herd_relatory.dart';
 
 export 'package:integrazoo/ui/screens/bovines_in_treatment_list_view.dart';
-export 'package:integrazoo/ui/screens/bovines_pregnant_list_view.dart';
-export 'package:integrazoo/ui/screens/bovines_reproducing_list_view.dart';
 export 'package:integrazoo/ui/screens/breeders_list_view.dart';
 export 'package:integrazoo/ui/screens/finishes_list_view.dart';
 export 'package:integrazoo/ui/screens/treatments_list_view.dart';
--- lib/ui/screens/relatories/herd_relatory.dart
+++ lib/ui/screens/relatories/herd_relatory.dart
@@ -2,9 +2,7 @@ import 'dart:developer'; // ignore: unused_import
 
 import 'package:flutter/material.dart';
 
-import 'package:integrazoo/ui/screens/bovines_in_treatment_list_view.dart';
-import 'package:integrazoo/ui/screens/bovines_pregnant_list_view.dart';
-import 'package:integrazoo/ui/screens/bovines_reproducing_list_view.dart';
+import 'package:integrazoo/ui/screens.dart';
 
 
 class HerdRelatory extends StatefulWidget {
@@ -25,8 +23,8 @@ class _HerdRelatory extends State<HerdRelatory> {
       length: 3,
       child: Column(
         children: [
-          TabBar(tabs: [ Tab(text: "Em Tratamento"), Tab(text: "Fêmeas Reproduzindo"), Tab(text: "Fêmeas Prenhes") ]),
-          Expanded(child: TabBarView(children: [ BovinesInTreatmentListView(), BovinesReproducingListView(), BovinesPregnantListView() ])),
+          TabBar(tabs: [ Tab(text: "Em Tratamento") ]),
+          Expanded(child: TabBarView(children: [ BovinesInTreatmentListView() ])),
         ],
       ),
     );
